6bf5c2cfcd27574893eae7d9d6a3a5134b472043
tanushkumar802 <84520701+tanushkumar802@users.noreply.github.com>
2021-10-07 21:14:06 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,20 @@
+class Box extends BaseClass {
+    constructor(x, y, width, height){
+      super(x,y,width,height);
+      this.image = loadImage("sprites/wood1.png");
+  block1= new Block(330,235,30,40);
+  block2= new Block(360,235,30,40);
+  block3= new Block(390,235,30,40);
+  block4= new Block(420,235,30,40);
+  block5= new Block(450,235,30,40);
+  block6= new Block(360,195,30,40);
+  block7= new Block(390,195,30,40);
+  block8= new Block(420,195,30,40);
+  block9= new Block(360,155,30,40);
+  block10= new Block(390,155,30,40);
+  block11= new Block(420,155,30,40);
+  block12= new Block(390,235,30,40);
+  block13= new Block(420,235,30,40);
+     }
+  
+  };
---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(800,400);
+    createSprite(400, 200, 50, 50);
+  
+}
+  
+  function draw() {
+    background(255,255,255);  
+    function keyPressed(){
+        
+    }
+    slingShot.add(this.polygon);
+    drawSprites();
+  }
